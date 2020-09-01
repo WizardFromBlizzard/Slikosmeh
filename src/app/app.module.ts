5c2ea3cff74912fc72aafff61d42eb4f16bf4e10
WizardFromBlizzard <tekilaaliket@hotmail.com>
2020-09-01 14:46:20 +0200

    first commit and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GeneratorComponent
   ],
   imports: [
     BrowserModule
